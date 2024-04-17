cf4dd99fd30e1a672cc4699e8f35e6e31388e539
Jonathan Morales Velez <jonathan.morales-velez@spendesk.com>
2024-04-17 21:34:52 +0200

    updated home page and site name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Jonathanmv Blog";
+export const SITE_DESCRIPTION =
+  "a personal blog about content generation with AI";
