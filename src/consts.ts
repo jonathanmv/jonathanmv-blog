2d9edd870a1a583f053845cdc70802165c0842cd
Jonathan Morales Velez <jonathan.morales-velez@spendesk.com>
2024-04-17 21:55:19 +0200

    update links

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Jonathanmv Blog";
+export const SITE_TITLE = "jonathanmv Blog";
 export const SITE_DESCRIPTION =
   "a personal blog about content generation with AI";
